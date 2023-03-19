40a0840de1bf7b08f1774b1d00e2b5b2319570af
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2023-03-18 20:59:20 -0500

    Add mongo client

---
+++ backend/database.js
@@ -0,0 +1,10 @@
+const {MongoClient} = require('mongodb');
+
+
+
+module.exports = {
+    connectToDb: () => {
+        MongoClient.connect('mongodb://localhost:27017/')
+    },
+    getDb: () => {}
+}
